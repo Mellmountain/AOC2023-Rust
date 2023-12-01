dfd141f2548c67ddb831cabf8bf4334480fe08f7
Jonas Mellberg <jonas.mellberg@teliacompany.com>
2023-12-01 16:54:59 +0100

    draft code

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world");
+    let mut input = include_str!("./sample.txt").lines();
+    let mut sum = 0;
+    for line in input {
+        sum += parse_calibration(line);
+    }
+    println!("{}", sum);
+}
+
+fn parse_calibration(input: &str) -> u32 {
+    let mut ten = 0;
+    let mut one = 0;
+    for c in input.chars() {
+        if c.is_numeric() {
+            ten = char::to_digit(c, 10).unwrap();
+            break;
+        }
+    }
+    for c in input.chars().rev() {
+        if c.is_numeric() {
+            one = char::to_digit(c, 10).unwrap();
+            break;
+        }
+    }
+    return (ten + one);
 }
