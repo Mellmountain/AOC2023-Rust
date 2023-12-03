b0cbcc201d07cae37c707aaa8189ff8613a839c0
Jonas Mellberg <jonas.mellberg@gmail.com>
2023-12-03 12:13:46 +0100

    solved part 1 using nom

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,18 +1,73 @@
 use std::collections::HashMap;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{
+        self, alpha1, digit1, line_ending,
+    },
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+#[derive(Debug)]
+struct Game<'a> {
+    id: &'a str,
+    rounds: Vec<Vec<Cube<'a>>>,
+}
+#[derive(Debug)]
+struct Cube<'a> {
+    color: &'a str,
+    amount: u32,
+}
 
-struct Game {
-    red:   i32,
-    blue:  i32,
-    green: i32,
-}
-impl Game {
-    fn new(red: i32, blue: i32, green: i32) -> Self {
-        Game {red,  blue, green}
+impl<'a> Game<'a> {
+
+    fn is_valid(&self, limits: &HashMap<&str, u32>) -> Option<u32> {
+        self.rounds.iter().all(|round| {
+            round.iter().all(|shown_cube| {
+                shown_cube.amount <= *limits
+                    .get(shown_cube.color)
+                    .expect("a valid color is always shown")
+            })
+        }).then_some(self.id.parse::<u32>().expect("should parse"))
     }
-    
 }
+
+//3 blue
+fn cube(input: &str) -> IResult<&str, Cube> {
+    let (input, (amount, color)) = 
+        separated_pair(complete::u32, tag(" "), alpha1)(input,)?;
+    Ok((input, Cube {color, amount}))
+}
+
+// 3 blue, 4 red
+fn round(input: &str) -> IResult<&str, Vec<Cube>> {
+    let (input, cubes) = separated_list1(tag(", "), cube)(input)?;
+    Ok((input, cubes))
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn game(input: &str) -> IResult<&str, Game> {
+    let (input, id) = preceded(tag("Game "), digit1)(input)?;
+    let (input, rounds) = preceded(tag(": "), separated_list1(tag("; "), round),)(input)?;
+    Ok((input, Game {id, rounds}))
+}
+
+fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
+    let (input, games) = 
+        separated_list1(line_ending, game)(input)?;
+    
+    Ok((input, games))
+}
+
 
 fn main() {
-    let mut input = include_str!("./input.txt").lines();
-    let mut games: HashMap<i32, Game> = HashMap::new();    
+    let input = include_str!("./input.txt");
+    let limits = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14 )]);
+    let games = parse_games(input).expect("Parse MF");
+    let part1 = games.1.iter().filter_map(|game| game.is_valid(&limits)).sum::<u32>();
+    println!("Part 1: {}", part1);
+    //dbg!(games);
 }
