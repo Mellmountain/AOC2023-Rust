a3c013995be10c15ba1ea9fa0dff246b2881cafe
Jonas Mellberg <jonas.mellberg@gmail.com>
2023-12-03 22:39:11 +0100

    part1 day03

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,3 +1,66 @@
+use std::collections::HashSet;
+
+#[derive(Default)]
+struct Day03 {
+    numbers: Vec<PartNumber>,
+    symbols: HashSet<(i32, i32)>,
+}
+
+impl Day03 {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
 fn main() {
-    let input = include_str!("./input.txt");
+    let input = include_str!("./input.txt").lines();
+    let mut day03 = Day03::new();
+    let mut curr_part: Option<PartNumber> = None;
+    for (y, line) in input.into_iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c.is_ascii_digit() {
+                if let Some(ref mut num) = curr_part {
+                    num.add_digit(x as i32, y as i32, c);
+                } else {
+                    curr_part = Some(PartNumber::new(x as i32, y as i32, c));
+                }
+            } else {
+                if let Some(num) = curr_part.take() {
+                    day03.numbers.push(num);
+                }
+                if c != '.' {
+                    day03.symbols.insert((x as i32, y as i32));
+                }
+            }
+        }
+    }
+    let part1 = day03.numbers
+                            .iter()
+                            .filter(|num| num.points.intersection(&day03.symbols).next().is_some())
+                            .map(|num| num.value)
+                            .sum::<i32>();
+    println!("{}", part1);
+
+}
+#[derive(Debug)]
+struct PartNumber {
+    value: i32,
+    points: HashSet<(i32, i32)>,
+}
+
+impl PartNumber {
+    fn new(x: i32, y: i32, c: char) -> Self {
+        let points = HashSet::from([
+            (x - 1, y), (x - 1, y - 1), (x - 1, y + 1), //Behind
+            (x, y + 1), (x, y - 1),                     //Up-down
+            (x + 1, y), (x + 1, y - 1), (x + 1, y + 1)  //Ahead
+        ]);
+        Self {
+            value: (c as u8 - b'0') as i32,
+            points
+        }
+    }
+    fn add_digit(&mut self, x: i32, y: i32, c: char) {
+        self.value = self.value * 10 + (c as u8 - b'0') as i32;
+        self.points.extend([(x + 1, y), (x + 1, y - 1), (x + 1, y + 1)]);
+    }
 }
